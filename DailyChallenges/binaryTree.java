4e1115842a94fb762c8544412b1ceebb6bfcec96
Pravanshu Pati <pravanshu.pati2020@vitbhopal.ac.in>
2023-01-09 22:35:36 +0530

    Initial commit

---
+++ DailyChallenges/binaryTree.java
@@ -0,0 +1,42 @@
+package DailyChallenges;
+
+import java.util.ArrayList;
+
+/* 144. Binary Tree Preorder Traversal
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100 
+
+*/
+
+public class binaryTree {
+    ArrayList<Integer> list=new ArrayList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if(root==null) return list;
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+        return list;
+    }
+    
+}
