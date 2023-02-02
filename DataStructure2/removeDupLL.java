386578cd63566ef38395a1e7a8c778aa36b9501e
Pravanshu Pati <pravanshu.pati2020@vitbhopal.ac.in>
2023-02-02 22:23:18 +0530

    Initial commit

---
+++ DataStructure2/removeDupLL.java
@@ -0,0 +1,49 @@
+package DataStructure2;
+
+/* 82. Remove Duplicates from Sorted List II
+
+Given the head of a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list. Return the linked list sorted as well.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,3,4,4,5]
+Output: [1,2,5]
+Example 2:
+
+
+Input: head = [1,1,1,2,3]
+Output: [2,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+
+*/
+
+public class removeDupLL {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next=head;
+        head=dummy;
+        while(head.next!=null && head.next.next!=null){
+            if(head.next.val == head.next.next.val){
+                int val = head.next.val;
+                while(head.next!=null && head.next.val==val){
+                    head.next=head.next.next;
+                }
+            } else{
+                head=head.next;
+            }
+        }
+        return dummy.next;
+    }
+}
