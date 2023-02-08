89889c9141e02f5fe65b9ab5c9a87c74e618f0f0
Pravanshu Pati <pravanshu.pati2020@vitbhopal.ac.in>
2023-02-08 22:36:29 +0530

    Initial commit

---
+++ DataStructure2/kthSmallestElement.java
@@ -0,0 +1,53 @@
+package DataStructure2;
+
+/* 230. Kth Smallest Element in a BST
+
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is n.
+1 <= k <= n <= 104
+0 <= Node.val <= 104
+ 
+
+Follow up: If the BST is modified often (i.e., we can do insert and delete operations) and you need to find the kth smallest frequently, how would you optimize?
+
+*/
+
+public class kthSmallestElement {
+    static int count;
+    static int res;
+    public int kthSmallest(TreeNode root, int k) {
+        count = 0;
+        res = 0;
+        inorder(root,k);
+        return res;
+    }
+    private void inorder(TreeNode root, int k){
+        if(root == null)
+            return;
+        
+        inorder(root.left,k);
+        count++;
+        if(count == k){
+            res = root.val;
+            return;
+        }
+        inorder(root.right,k);
+    }
+}
