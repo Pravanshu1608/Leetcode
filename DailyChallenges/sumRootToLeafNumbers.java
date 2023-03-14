a93f3d3d0c65b989f6dfb159a317c68b127a68eb
Pravanshu Pati <pravanshu.pati2020@vitbhopal.ac.in>
2023-03-14 22:33:54 +0530

    Sum Root To Leaf Numbers

---
+++ DailyChallenges/sumRootToLeafNumbers.java
@@ -0,0 +1,71 @@
+package DailyChallenges;
+
+/* 129. Sum Root to Leaf Numbers
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+
+*/
+
+public class sumRootToLeafNumbers {
+    public int sumNumbers(TreeNode root) {
+        return sumNumbersUtils(root, new ArrayList<>(), new int[1]);
+ }
+
+  private static int sumNumbersUtils(TreeNode root, List<Integer> sum , int[] totalSum)
+ {
+     if(root  == null)
+     {
+         return 0;
+     }
+     sum.add(root.val);
+     if(root.left == null && root.right == null)
+     {
+         for (int i = 0; i < sum.size(); i++) {
+             totalSum[0] += (int)(Math.pow(10,sum.size()-1-i) )* sum.get(i);
+         }
+     }
+
+     sumNumbersUtils(root.left, sum, totalSum );
+
+     sumNumbersUtils(root.right, sum, totalSum);
+     sum.remove(sum.size()-1);
+
+     return totalSum[0];
+ }
+}
