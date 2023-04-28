4fa14ba071a41bee6b7e4ca8c3c28c646c6f3a1e
Pravanshu Pati <pravanshu.pati2020@vitbhopal.ac.in>
2023-04-28 22:23:21 +0530

    Similar String Groups

---
+++ DailyChallenges/similarStringGroups.java
@@ -0,0 +1,65 @@
+package DailyChallenges;
+
+/* 839. Similar String Groups
+
+Two strings X and Y are similar if we can swap two letters (in different positions) of X, so that it equals Y. Also two strings X and Y are similar if they are equal.
+
+For example, "tars" and "rats" are similar (swapping at positions 0 and 2), and "rats" and "arts" are similar, but "star" is not similar to "tars", "rats", or "arts".
+
+Together, these form two connected groups by similarity: {"tars", "rats", "arts"} and {"star"}.  Notice that "tars" and "arts" are in the same group even though they are not similar.  Formally, each group is such that a word is in the group if and only if it is similar to at least one other word in the group.
+
+We are given a list strs of strings where every string in strs is an anagram of every other string in strs. How many groups are there?
+
+ 
+
+Example 1:
+
+Input: strs = ["tars","rats","arts","star"]
+Output: 2
+Example 2:
+
+Input: strs = ["omv","ovm"]
+Output: 1
+ 
+
+Constraints:
+
+1 <= strs.length <= 300
+1 <= strs[i].length <= 300
+strs[i] consists of lowercase letters only.
+All words in strs have the same length and are anagrams of each other.
+
+*/
+
+public class similarStringGroups {
+    
+}public int numSimilarGroups(String[] A) {
+    if(A.length < 2) return A.length;
+    int res = 0;
+    for(int i=0;i<A.length;i++){
+        if(A[i] == null) continue;
+        String str = A[i];
+        A[i] = null;
+        res++;
+        dfs(A,str);
+    }
+    return res;
+}
+public void dfs(String[] arr,String str){
+    for(int i=0;i<arr.length;i++){
+        if(arr[i] == null) continue;
+        if(helper(str,arr[i])){// both string str and arr[i] belong in same group
+            String s = arr[i];
+            arr[i] = null;
+            dfs(arr,s);
+        }
+    }
+}
+public boolean helper(String s,String t){
+    int res = 0, i = 0;
+    while(res <= 2 && i < s.length()){
+        if(s.charAt(i) != t.charAt(i)) res++;
+        i++;
+    }
+    return res == 2 || res == 0;
+}
