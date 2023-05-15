f81f9ed8c8e67dd04d4a30c2c47bf705fbb91b87
Pravanshu Pati <pravanshu.pati2020@vitbhopal.ac.in>
2023-05-15 22:14:40 +0530

    Swapping Nodes In A Linked List

---
+++ DailyChallenges/swappingNodesInALinkedList.java
@@ -0,0 +1,50 @@
+package DailyChallenges;
+
+/* 1721. Swapping Nodes in a Linked List
+
+You are given the head of a linked list, and an integer k.
+
+Return the head of the linked list after swapping the values of the kth node from the beginning and the kth node from the end (the list is 1-indexed).
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [1,4,3,2,5]
+Example 2:
+
+Input: head = [7,9,6,6,7,8,3,0,9,5], k = 5
+Output: [7,9,6,6,8,7,3,0,9,5]
+ 
+
+Constraints:
+
+The number of nodes in the list is n.
+1 <= k <= n <= 105
+0 <= Node.val <= 100
+
+*/
+
+public class swappingNodesInALinkedList {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode leftNode = head, rightNode = head;
+        
+        for (int i = 1; i < k; i++) {
+            leftNode = leftNode.next;
+        }
+        
+        ListNode current = leftNode;
+        while (current.next != null) {
+            current = current.next;
+            rightNode = rightNode.next;
+        }
+        
+        int temp = leftNode.val;
+        leftNode.val = rightNode.val;
+        rightNode.val = temp;
+        
+        return head;
+    }
+}
